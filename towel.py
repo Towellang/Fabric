80296185073408ce6eb6331675ef063ff29c1e09
Weloxux <weloxux@glowbug.nl>
2015-12-10 23:52:25 +0100

    Add =, >, and <

--- towel.py
+++ towel.py
@@ -7,6 +7,7 @@ from string import join
 STACK = []
 LOOPS = [] # Loops are stacks too
 PNT = 0
+BREAK = False # Break mode
 
 ENV = {
 	"[": lambda: LOOPS.append(PNT),
@@ -24,7 +25,13 @@ ENV = {
 	"-": lambda: push(-1 * STACK.pop() + STACK.pop()),
 	"put": lambda: sys.stdout.write( str( STACK.pop() ) ),
 	"chr": lambda: sys.stdout.write( str( chr( STACK.pop() ) ) ),
+	"=": lambda: logic(True) if STACK.pop() == STACK.pop() else logic(False),
+	">": lambda: logic(True) if STACK.pop() > STACK.pop() else logic(False),
+	"<": lambda: logic(True) if STACK.pop() < STACK.pop() else logic(False),
+	"swap": lambda: swap(),
+	"clone": lambda: STACK.extend(STACK),
 	"grab": lambda: push(raw_input("")),
+	"break": lambda: breakmode(),
 #	"=": lambda: if STACK.pop() != STACK.pop(): PNT += 1,
 	"end": lambda: leave()
 }
@@ -45,9 +52,17 @@ def loop():
 	else:
 		PNT = LOOPS[-1]
 
-def panic(msg):
-	print("ERROR: " + msg + " on instruction #" + str(PNT + 1) + " (" + code[PNT] + ")")
-	sys.exit(1)
+def breakmode():
+	global BREAK
+	BREAK = True
+
+def swap():
+	STACK[-1], STACK[-2] = STACK[-2], STACK[-1]
+
+def logic(bvar):
+	global PNT
+	if bvar != True:
+		PNT += 1
 
 def prep(rawcode):
 	for k,v in enumerate(rawcode):
@@ -57,20 +72,26 @@ def prep(rawcode):
 	return rawcode.split(" ")
 
 def interpret(code):
-	global STACK, LOOPS, PNT
+	global STACK, LOOPS, PNT, BREAK
 	while PNT < len(code):
 		word = code[PNT]
-		if word == "":
-			PNT += 1
-			continue
-		if word.startswith("\"") and word.endswith("\""):
-			push(word)
-		elif word in ENV.keys():
-			ENV[word]()
+		if BREAK:
+			if word == "]":
+				BREAK = False
 		else:
-			try: int(word)
-			except: panic("Unknown command or broken value")
-			else: push(word)
+			if word == "":
+				PNT += 1
+				continue
+			if word.startswith("\"") and word.endswith("\""):
+				push(word)
+			elif word in ENV.keys():
+				ENV[word]()
+			elif word == "_SLEN":
+				push(len(STACK))
+			else:
+				try: int(word)
+				except: panic("Unknown command or broken value")
+				else: push(word)
 
 		PNT += 1
 
@@ -81,6 +102,10 @@ def repl():
 		code.append(prep(raw_input(">>> ").split(" ")))
 		interpret(code)
 	print("Go away! No REPL here!")
+
+def panic(msg):
+	print("ERROR: " + msg + " on instruction #" + str(PNT + 1) + " (" + code[PNT] + ")")
+	sys.exit(1)
 
 def leave():
 	print # Trailing newline to fix command prompt
